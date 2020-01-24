7bcba8594009c1d7113731b7a0f536d7701a8ad1
Ogie Sado <sado.ogie@gmail.com>
2020-01-24 13:51:58 +0000

    duplicating quote

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ export default function App() {
   return (
     <div className="App">
       <h2>Someone hit your girl's phone up, I will tell you if it was me</h2>
+      <h2>Someone hit your girl's phone up, I will tell you if it was me</h2>
     </div>
   );
 }
